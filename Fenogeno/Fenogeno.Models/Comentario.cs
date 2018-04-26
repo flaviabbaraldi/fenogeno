7ff944f3236201bd0762a1a28bbd532105b30e3c
tomlipe <tomlipe15@gmail.com>
2018-04-26 20:00:50 -0300

    Comentario

---
+++ Fenogeno/Fenogeno.Models/Comentario.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fenogeno.Models
+{
+   public class Comentario
+    {
+        public int Id { get; set; }
+        public Noticia Noticia { get; set; }
+        public Usuario Usuario { get; set; }
+        public DateTime DataHora { get; set; }
+        public string Texto { get; set; }
+    }
+}
