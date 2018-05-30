d83b6572cef4d74cb99c9b32dfe7e6a04cc84797
tomlipe <tomlipe15@gmail.com>
2018-05-30 19:55:52 -0300

    sair + banco

--- Fenogeno/Fenogeno.WebUI/VerEspecialista.aspx.designer.cs
+++ Fenogeno/Fenogeno.WebUI/VerEspecialista.aspx.designer.cs
@@ -49,13 +49,13 @@ namespace Fenogeno.WebUI {
         protected global::System.Web.UI.WebControls.TextBox txtDuvida;
         
         /// <summary>
-        /// btnEnviar control.
+        /// btnSair control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Button btnEnviar;
+        protected global::System.Web.UI.WebControls.Button btnSair;
         
         /// <summary>
         /// btnCancelar control.
