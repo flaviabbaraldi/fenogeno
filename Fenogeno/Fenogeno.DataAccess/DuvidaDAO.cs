a73b8c77619670bbdf7ef45674e7ed9e3f7dd9ed
flaviabbaraldi <flaviabbaraldi@hotmail.com>
2018-06-04 19:26:01 -0300

    Finalização classe DuvidaDAO

--- Fenogeno/Fenogeno.DataAccess/DuvidaDAO.cs
+++ Fenogeno/Fenogeno.DataAccess/DuvidaDAO.cs
@@ -77,6 +77,49 @@ namespace Fenogeno.DataAccess
 
             return lstDuvidas;
         }
+        public List<Duvida> BuscarPorEspecialista(int codDuvida)
+        {
+            var lstDuvidas = new List<Duvida>();
+
+            using (SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["Db"].ConnectionString))
+            {
+                string strSQL = @"SELECT * FROM DUVIDA WHERE ID_ESPECIALISTA = @ID_ESPECIALISTA;";
+
+                using (SqlCommand cmd = new SqlCommand(strSQL))
+                {
+                    conn.Open();
+                    cmd.Connection = conn;
+                    cmd.CommandText = strSQL;
+
+                    var dataReader = cmd.ExecuteReader();
+                    var dt = new DataTable();
+                    dt.Load(dataReader);
+                    conn.Close();
+
+                    foreach (DataRow row in dt.Rows)
+                    {
+                        var duvida = new Duvida()
+                        {
+                            Id = Convert.ToInt32(row["ID"]),
+                            Especialista = new Especialista()
+                            {
+                                Cod = Convert.ToInt32(row["ID_ESPECIALISTA"]),
+                            },
+                            Usuario = new Usuario()
+                            {
+                                Id = Convert.ToInt32(row["ID_USUARIO"]),
+                            },
+                            DataHora = Convert.ToDateTime(row["DATAHORA"]),
+                            Texto = row["TEXTO"].ToString()
+                        };
+
+                        lstDuvidas.Add(duvida);
+                    }
+                }
+            }
+
+            return lstDuvidas;
+        }
 
     }
 }
