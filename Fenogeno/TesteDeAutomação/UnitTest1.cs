343c960ed39d2392aeb8f825f58099ed5707d9dc
tomlipe <tomlipe15@gmail.com>
2018-06-08 21:21:59 -0300

    Fim do plano de teste

---
+++ Fenogeno/TesteDeAutomação/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Fenogeno.Models;
+
+namespace TesteDeAutomação
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            
+        }
+    }
+}
